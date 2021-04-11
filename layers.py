dd8c0454775949ea1cc029618e23ab1e3d637074
Patrick Manalu <pwillyam@gmail.com>
2021-04-12 00:46:09 +0200

    Layers used in the model (V1.0)
    -conv2dx returns a convolution output processed with ReLU.
    -maxpool is a max pooling algorithm.
    -dense is a representation of the neural networks using ReLU activation function.
    
    *for more information refer to Tensorflow 2.4.1 API Documentation

---
+++ layers.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+# Functions used for the layers in CNN
+
+def conv2dx(inp, filt, stride_s) :
+  conx = tf.nn.conv2d(inp, filters = filt, strides = [1, stride_s, stride_s, 1], padding = "SAME")
+  return tf.nn.relu(conx)
+
+def maxpool(inp, k, stride_s) :
+  poolx = tf.nn.max_pool2d(inp,ksize=[1, k, k, 1], strides=[1, stride_s, stride_s, 1],padding = "VALID")
+  return poolx
+
+def dense(inp, weights) :
+  densex = tf.nn.relu(tf.matmul(inp, weights))
+  return tf.nn.dropout(densex, 0.5)
+
+
+
+
