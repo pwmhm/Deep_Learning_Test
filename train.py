0bd9780b162a3e6d91245d0b01c1e363ca2bf329
Patrick Manalu <pwillyam@gmail.com>
2021-04-12 21:23:19 +0200

    Train 1.1
    Changes :
    -Removed tf-ds dependency
    -Added saving functionality for model weights and logs

--- train.py
+++ train.py
@@ -1,29 +1,23 @@
 import tensorflow as tf
-import tensorflow_datasets as tfds
 import main as m
-import dataset as ds
+import pickle
+from datetime import date
 
 
-#import cats vs dogs dataset
-
+#initialize some parameters
+learning_rate = 0.0008   #standard learning rate2
+epoch         = 20      #massive data
+batch_size    = 2
+dropout_rate  = 0.7
 dataset_name = 'cats_vs_dogs' 
 
-ds_raw = tfds.load(
-    name=dataset_name,
-    split = 'train',
-    with_info =False,
-    shuffle_files=False)
+#import cats vs dogs dataset
+train_dataset = tf.data.experimental.load("processed_dataset/train/", (tf.TensorSpec(shape=(300, 300, 3), dtype=tf.float32, name=None),
+ tf.TensorSpec(shape=(), dtype=tf.int64, name=None)), compression="GZIP")
 
-ds_raw = ds_raw.take(5000)
+train_dataset = train_dataset.batch(batch_size)
 
-batch_size = 10
-divider = len(ds_raw)/2
 
-train_dataset, test_dataset = ds.data_preprocessing(ds_raw, batch_size, divider)
-
-#initialize some training functions
-learning_rate = 1e-03   #standard learning rate2
-epoch         = 20      #massive data
 
 
 def loss(pred, target) :
@@ -32,11 +26,17 @@ def loss(pred, target) :
 optimizer = tf.optimizers.Adam( learning_rate ) 
 
 training_model = m.cdmodel()
+str_date = "logs/training-" + str(date.today()) +".txt"
+text_file = open(str_date, "wt")
+text_file.write("Parameters : \nLearning_rate = {0} ,Epoch : {1} ,Batch Size : {2},Data Length : {3}\n\n".format(learning_rate, epoch, batch_size, len(train_dataset)))
+text_file.write("Current Epoch,Current Loss\n")
 
 for i in range(epoch) :
-    print("Epoch : %d" % epoch)
-    print("Current Epoch : %d" % i)
+    j=0
     for inputs in train_dataset:
+        if j%4 == 0 :
+            with open('saved_weights.pickle', 'wb') as handle:
+                pickle.dump(training_model.model_weights, handle)
         #extract the inputs
         image, label = inputs[0], inputs[1]
 
@@ -48,4 +48,11 @@ for i in range(epoch) :
             closs = loss(training_model(image_input_mapped), tf.one_hot(label, depth=2))
         g = Tape.gradient(closs, training_model.model_weights)
         optimizer.apply_gradients(zip(g, training_model.model_weights))
-        tf.print(tf.reduce_mean(closs))
+        j += 1
+        text_file.write("{0},{1}\n".format(i, tf.reduce_mean(closs)))
+        tf.print(i, tf.reduce_mean(closs))
+
+with open('saved_weights.pickle', 'wb') as handle:
+    pickle.dump(training_model.model_weights, handle)
+    print("SAVED!")
+text_file.close()
